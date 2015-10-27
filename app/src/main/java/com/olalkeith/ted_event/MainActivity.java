16314cb40d9349f619869393bc270010690915e1
OlalKeith <jkeneth.jk@gmail.com>
2015-10-27 20:30:16 +0300

    socialmedia

--- app/src/main/java/com/olalkeith/ted_event/MainActivity.java
+++ app/src/main/java/com/olalkeith/ted_event/MainActivity.java
@@ -1,13 +1,27 @@
 package com.olalkeith.ted_event;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a  = new Intent(MainActivity.this, event_details.class );
+                startActivity(a);
+
+                finish();
+            }
+        });
     }
 }
