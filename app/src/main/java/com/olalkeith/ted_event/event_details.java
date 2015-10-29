9d4548ca5d9374ae0e535593659778d9f7f65027
OlalKeith <jkeneth.jk@gmail.com>
2015-10-30 00:49:23 +0300

    speakers activity

--- app/src/main/java/com/olalkeith/ted_event/event_details.java
+++ app/src/main/java/com/olalkeith/ted_event/event_details.java
@@ -1,13 +1,30 @@
 package com.olalkeith.ted_event;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class event_details extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+
+                                        }
+                                    });
+
+                Intent a = new Intent(event_details.this, Speakers.class);
+        startActivity(a);
+
+        finish();
+
     }
 }
